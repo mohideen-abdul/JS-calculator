03a6f92436d7e68b471bd4f23eb48ea67917e945
mohideen-abdul <155938095+mohideen-abdul@users.noreply.github.com>
2024-07-28 16:00:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let os=document.getElementById('os');
+
+function Clear()
+{
+   os.value="";
+}
+
+function del()
+{
+   os.value=os.value.slice(0,-1);
+}
+function display(txt)
+{
+   os.value=os.value+txt;
+   console.log(os.value);
+}
+function calculate()
+{
+  os.value=eval(os.value);
+}
